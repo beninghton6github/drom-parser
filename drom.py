71d125df68e99aec0852c5c58505296356994c3f
beninghton6github <41121388+beninghton6github@users.noreply.github.com>
2018-08-03 23:11:54 +0700

    Added multiprocessing on parse_hosts_count

--- drom.py
+++ drom.py
@@ -3,28 +3,30 @@
 from urllib.request import urlopen
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
+
 
 # Возвращает страницу и кол-во хозяев, если их меньше 2 и тачка не в розыске, и без штрафов.
 def parse_hosts_count(page_url):
     page = urlopen(page_url)
     soup = BeautifulSoup(page, 'html.parser')
-    
+
     # Парисм по тегу div'a
     hosts_field = soup.find_all(class_="b-media-cont b-media-cont_no-clear b-media-cont_bg_gray b-media-cont_modify_md b-random-group b-random-group_margin_b-size-xss")
-    
+    if hosts_field == []:
+        return
+
     # Если тег есть, у нас список из одного элемента. Если лист пустой - просто pass с IndexError.
     # И по результату обрабатываем следующие теги, внутри него. Они называются одинакого, но у них есть определенный порядок всегда, в нем и отлавливаю.
     # По сути, если имя останется, но порядок изменится, работать перестанет. Но пока работает)
-    try:
-        criminal_search = hosts_field[0].find_all("div", {"class":"b-flex__item"})[3].text.strip()
-        restricts = hosts_field[0].find_all("div", {"class":"b-flex__item"})[5].text.strip()
-        hosts_count = hosts_field[0].find_all("div", {"class":"b-flex__item"})[1].text
+    criminal_search = hosts_field[0].find_all("div", {"class": "b-flex__item"})[3].text.strip()
+    restricts = hosts_field[0].find_all("div", {"class": "b-flex__item"})[5].text.strip()
+    hosts_count = hosts_field[0].find_all("div", {"class": "b-flex__item"})[1].text
 
-        if criminal_search == "нет" and restricts == "нет" and int(hosts_count) <= 2:
-            print("{} Хозяев: {}".format(page_url, hosts_count))
+    if criminal_search == "нет" and restricts == "нет" and int(hosts_count) <= 2:
+         print("{} Хозяев: {}".format(page_url, hosts_count))
 
-    except IndexError:
-        pass
+
 
 # Получаем список урлов со всех страниц по запросу с дрома.
 def get_url_list():
@@ -36,11 +38,11 @@ def get_url_list():
         # Самый простой get query, пока не раскидал параметры запроса по переменным. Ну не суть.
         # По страницам бегаем просто подставляя номер новой, пока что то отдается. 
         # Т.е. может открыться хоть 100500 страница, ошибки не будет, просто она будет без нужного контента.
-        query = "https://kemerovo.drom.ru/auto/page{}/?distance=100&minprice=250000&maxprice=320000&minyear=2005&transmission=2&privod=1&ph=1&inomarka=1&order=price&order_d=desc&w=2&unsold=1&go_search=2".format(page_number)
+        query = "https://kemerovo.drom.ru/auto/page{}/?distance=100&minprice=220000&maxprice=320000&minyear=2005&transmission=2&privod=1&ph=1&inomarka=1&order=price&order_d=desc&w=2&unsold=1&go_search=2".format(page_number)
         response = requests.get(query)
         soup = BeautifulSoup(response.text, 'html.parser')
         findings = soup.find_all(class_="b-media-cont b-media-cont_modifyMobile_sm")
-        
+
         # Если пусто, значит наши страницы кончились.
         if len(findings) == 0:
             break
@@ -57,8 +59,9 @@ def main():
     url_list = get_url_list()
     print("Найдено {} объявлений".format(len(url_list)))
 
-    for url in url_list:
-        parse_hosts_count(url)
+    with Pool(5) as p:
+        p.map(parse_hosts_count, url_list)
+
 
 
 
